c4454d2f3085e54e7d29bf2acc5df26a01de7360
Jack Danger Canty <jackdanger@squareup.com>
2015-12-02 16:33:22 -0800

    Show the state of all goroutines
    Allows you to inspect the state of an application without interrupting
    the program. Uses the QUIT signal which normally does this but also
    terminates the app.
    Pro-tip: Use your keyboard's CTRL-\ key to send this signal to the
    foregrounded program.

---
+++ print-goroutines.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+	"runtime/pprof"
+	"syscall"
+)
+
+func init() {
+	var debugChan = make(chan os.Signal, 1)
+	signal.Notify(debugChan, syscall.SIGQUIT)
+	go func() {
+		select {
+		case <-debugChan:
+			pprof.Lookup("goroutine").WriteTo(os.Stdout, 1)
+		}
+	}()
+}
