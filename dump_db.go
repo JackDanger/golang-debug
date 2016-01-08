26c94c7cf6699e80501fa2a4c20be6f738193bd5
Jack Danger Canty <jackdanger@squareup.com>
2016-01-07 16:09:10 -0800

    adding print_rows

---
+++ dump_db.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+)
+
+func () main() {
+	dump(someDB, "SELECT * FROM some_table")
+}
+
+// PrintRows prints the results of a SQL query in json format
+func PrintRows(db *DB, sqlString string) (string, error) {
+	rows, err := db.Query(sqlString)
+	if err != nil {
+		return "", err
+	}
+	defer rows.Close()
+	columns, err := rows.Columns()
+	if err != nil {
+		return "", err
+	}
+	count := len(columns)
+	var tableData []map[string]interface{}
+	values := make([]interface{}, count)
+	valuePtrs := make([]interface{}, count)
+	for rows.Next() {
+		for i := 0; i < count; i++ {
+			valuePtrs[i] = &values[i]
+		}
+		rows.Scan(valuePtrs...)
+		entry := make(map[string]interface{})
+		for i, col := range columns {
+			var v interface{}
+			val := values[i]
+			b, ok := val.([]byte)
+			if ok {
+				v = string(b)
+			} else {
+				v = val
+			}
+			entry[col] = v
+		}
+		tableData = append(tableData, entry)
+	}
+	jsonData, err := json.Marshal(tableData)
+	if err != nil {
+		return "", err
+	}
+	fmt.Println(string(jsonData))
+	return string(jsonData), nil
+}
