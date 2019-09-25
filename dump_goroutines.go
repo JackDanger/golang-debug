ba0fc2d90a630d05059ea97fa35159399666cf47
Jack Danger <github@jackcanty.com>
2019-09-25 12:38:23 -0700

    Registering goroutine dumping with SIGPROF

--- dump_goroutines.go
+++ dump_goroutines.go
@@ -7,20 +7,30 @@ import (
 	"syscall"
 )
 
+// usage:
+//   kill -27 $pid_of_this_program
+// or
+//   kill -SIGPROF $pid_of_this_program
+func init() {
+	DumpGoroutinesOnSig(syscall.SIGPROF)
+}
+
+// DumpGoroutines prints the state of all goroutines to stdout
 func DumpGoroutines() {
 	pprof.Lookup("goroutine").WriteTo(os.Stdout, 1)
 }
 
+// DumpGoroutinesOnSig registers a signal handler for the SIGPROF ('25', on most
+// architectures) signal
 func DumpGoroutinesOnSig(sig os.Signal) {
 	var debugChan = make(chan os.Signal, 1)
 	signal.Notify(debugChan, sig)
 	go func() {
-		select {
-		case <-debugChan:
-			DumpGoroutines()
+		for {
+			select {
+			case <-debugChan:
+				DumpGoroutines()
+			}
 		}
 	}()
-}
-func DumpGoroutinesOnSigQUIT() {
-	DumpGoroutinesOnSig(syscall.SIGQUIT)
 }
